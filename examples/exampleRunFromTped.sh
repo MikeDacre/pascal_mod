0726106944ad290f227f81ff45132d0a291dcfe1
Mike Dacre <mike.dacre@gmail.com>
2017-06-08 20:00:51 -0700

    Adding all files and data

---
+++ examples/exampleRunFromTped.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./Pascal --pval=resources/gwas/EUR.CARDIoGRAM_2010_lipids.HDL_ONE.txt --chr=22 --customdir=examples/ --custom=EXAMPLE
+echo "NOTE: the EXAMPLE tped file is only for illustrative purposes and doesn't cover all of chromosome 22. most genes should not have a gene score."
+
---
+++ installers/README.txt
@@ -0,0 +1,2 @@
+Move these files into the root directory of Pascal, make sure gcc-fortran (or
+gfortran) is installed, and run bash ./installScript.sh
